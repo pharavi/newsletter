66402b0390e2ad00f342a4d8f0b4255cbd550a63
pharavi <132127616+pharavi@users.noreply.github.com>
2023-09-18 09:29:58 +0000

    Initial commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -48,15 +48,27 @@ export default async function (req, res) {
 }
 
 const getPrompt = (description) => {
-	return `For the below job description, suggest a career objective, list of skills and list of minimum 6 suitable work achievemnet
-	in a json format. You have to return only a JSON. Below is the expected JSON format:
-	{
-		"objective": "",
-	  "skills": [],
-	  "Achievements": []		
-	}
-	
-	Job Description:
-	${description}
-	`
+    return `For the data provided below, create a structured JSON response that will help build a CME Newsletter showcasing the positive impacts of the recent marketing initiatives taken by Tech Mahindra's CME marketing team. The JSON response should be structured as follows:
+
+{
+  "title": "",
+  "objectives": [""],
+  "actions": [""],
+  "accountsInfluenced": [""],
+  "assets": [""],
+  "impact": ""
 }
+
+Please follow the guidelines below for each section:
+
+- "title": Provide a crisp and engaging title suitable for a business newsletter, summarizing the overall impact of the marketing initiatives.
+- "objectives": List the main objectives, focusing on highlighting the positive impact of the marketing initiatives. Ensure the language is business-appropriate and aligned with the objectives of a CME Newsletter.
+- "actions": Mention the actions taken to achieve the objectives, ensuring to make the language exciting and inspiring.
+- "accountsInfluenced": Specify the names of the client or business accounts influenced, based on the data provided in the inputs.
+- "assets": Detail the creatives built for the initiative mentioned in the inputs, including any notable highlights or features.
+- "impact": Describe the overall impact of the actions undertaken, emphasizing how they have enhanced brand recognition and/or customer relations for Tech Mahindra. Be sure to highlight any revenue impacts and convey the high level of contribution the CME marketing team has made. Include any data available in the inputs to demonstrate the successful impact of their actions in the Communications, Media, and Entertainment industry globally.
+
+Data:
+${description}
+`;
+};
--- pages/index.js
+++ pages/index.js
@@ -1,121 +1,178 @@
 import React from "react";
 import Head from 'next/head';
-import toast, {Toaster} from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import Link from "next/link";
 
 
-const ListBox = ({items}) => (
-	<ol className={"list-decimal list-inside"}>
-		{
-			items.length > 0
-				? items.map((item, index) => <li key={index}>{item}</li>)
-				: <span className={"text-xs"}>No data to display</span>
-		}
-	</ol>
+const ListBox = ({ items }) => (
+    <ol className={"list-decimal list-inside"}>
+        {
+            items.length > 0
+                ? items.map((item, index) => <li key={index}>{item}</li>)
+                : <span className={"text-xs"}>No data to display</span>
+        }
+    </ol>
 )
 
 export default function Home() {
 
-	const [data, setData] = React.useState("")
-	const [loading, setLoading] = React.useState(false)
+    const [data, setData] = React.useState("")
+    const [loading, setLoading] = React.useState(false)
 
-	const [objective, setObjective] = React.useState("")
-	const [skills, setSkills] = React.useState([])
-	const [Achievements, setAchievements] = React.useState([])
-	const [error, setError] = React.useState("")
+    const [title, setTitle] = React.useState("")
+    const [objectives, setObjectives] = React.useState([])
+    const [actions, setActions] = React.useState([])
+    const [accountsInfluenced, setAccountsInfluenced] = React.useState([])
+    const [assets, setAssets] = React.useState([])
+    const [impact, setImpact] = React.useState("")
+    const [error, setError] = React.useState("")
 
-	const handleSubmit = async (e) => {
-		e.preventDefault()
-		setLoading(true)
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
 
-		try {
-			let response = await fetch("/api/generate", {
-				method: 'POST',
-				body: JSON.stringify({description: data}),
-				headers: {
-					"Content-type": "application/json"
-				}
-			})
+        try {
+            let response = await fetch("/api/generate", {
+                method: 'POST',
+                body: JSON.stringify({ description: data }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
 
-			if (response.ok) {
-				response = await response.json()
-				setObjective(response.result.objective)
-				setSkills(response.result.skills)
-				setAchievements(response.result.Achievements)
-				toast.success("Suggestions Generated")
-			} else {
-				response = await response.json()
-				toast.error(response.error.message)
-			}
-		} catch (error) {
-			console.error(error.message)
-			toast.error(error.message)
-		}
-		setLoading(false)
-	}
+            if (response.ok) {
+                response = await response.json()
+                setTitle(response.result.title)
+                setObjectives(response.result.objectives)
+                setActions(response.result.actions)
+                setAccountsInfluenced(response.result.accountsInfluenced)
+                setAssets(response.result.assets)
+                setImpact(response.result.impact)
+                toast.success("Newsletter Content Generated")
+            } else {
+                response = await response.json()
+                toast.error(response.error.message)
+            }
+        } catch (error) {
+            console.error(error.message)
+            toast.error(error.message)
+        }
+        setLoading(false)
+    }
 
-	return (
-		<>
-			<Head>
-				<title>Resume Builder</title>
-				<meta name="description" content="Generate resume suggestion from a job post or job title"/>
-				<meta name="viewport" content="width=device-width, initial-scale=1"/>
-				<link rel="icon" href="/favicon.ico"/>
-			</Head>
-			<main className={"bg-purple-100"}>
-				<div className={"border-b-2 text-4xl text-center py-5 bg-purple-700 text-zinc-50"}>
-					Resume Builder
-				</div>
-				<div className={"max-w-5xl mx-auto h-[calc(100vh-146px)] overflow-y-scroll"}>
-					<div className={"md:grid md:grid-cols-2 gap-4"}>
-						<div className={"p-5"}>
-							<form onSubmit={handleSubmit}>
-								<label htmlFor={"job-desc"} className={"text-lg font-bold text-zinc-500"}>
-									Paste Job Description Below:
-								</label>
-								<textarea
-									rows={10}
-									onChange={(e) => setData(e.target.value)}
-									value={data}
-									id={"job-desc"}
-									placeholder={"paste here..."}
-									className={"w-full p-5 mt-3 border-2 border-purple-500 rounded-md resize-none outline-0"}
-								/>
-								<div className={"text-right mt-2"}>
-									<button
-										className={"inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm rounded-md text-white bg-purple-500 hover:bg-purple-600 disabled:bg-purple-300 transition ease-in-out duration-150 disabled:cursor-not-allowed"}
-										type={"submit"}
-										disabled={loading}
-									>
-										{loading ? "Generating..." : "Generate"}
-									</button>
-								</div>
-							</form>
-						</div>
-						<div className={"p-5 h-full max-h-[calc(100vh-146px)] overflow-y-scroll"}>
+    const downloadContentAsTextFile = () => {
+        const content = `
+        Title: ${title}
+
+        Objectives:
+        ${objectives.join('\n  ')}
+
+        Actions:
+        ${actions.join('\n  ')}
+
+        Accounts Influenced:
+        ${accountsInfluenced.join('\n  ')}
+
+        Assets:
+        ${assets.join('\n  ')}
+
+        Impact:
+        ${impact}
+        `;
+
+        const blob = new Blob([content], { type: 'text/plain' });
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'newsletter_content.txt';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    return (
+        <>
+            <Head>
+                <title>CME Newsletter Builder</title>
+                <meta name="description" content="Generate CME newsletter content based on the input data" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={"bg-white-100"}>
+                <div className={"border-b-2 text-4xl text-center py-5 bg-purple-700 text-zinc-50"}>
+                    CME Newsletter Builder
+                </div>
+                <div className={"max-w-5xl mx-auto h-[calc(100vh-146px)]"}>
+                    <div className={"md:grid md:grid-cols-2 gap-4"}>
+                        <div className={"p-5"}>
+                            <form onSubmit={handleSubmit}>
+                                <label htmlFor={"data-input"} className={"text-lg font-bold text-zinc-500"}>
+                                    Provide Data for Newsletter:
+                                </label>
+                                <textarea
+                                    rows={10}
+                                    onChange={(e) => setData(e.target.value)}
+                                    value={data}
+                                    id={"data-input"}
+                                    placeholder={"paste here..."}
+                                    className={"w-full p-5 mt-3 border-2 border-purple-500 rounded-md resize-none outline-0"}
+                                />
+                                <div className={"text-right mt-2"}>
+                                    <button
+                                        className={"inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm rounded-md text-white bg-purple-500 hover:bg-purple-600 disabled:bg-purple-300 transition ease-in-out duration-150 disabled:cursor-not-allowed mr-2"}
+                                        type={"button"}
+                                        onClick={downloadContentAsTextFile}
+                                    >
+                                        Download Content
+                                    </button>
+                                    <button
+                                        className={"inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm rounded-md text-white bg-purple-500 hover:bg-purple-600 disabled:bg-purple-300 transition ease-in-out duration-150 disabled:cursor-not-allowed"}
+                                        type={"submit"}
+                                        disabled={loading}
+                                    >
+                                        {loading ? "Generating..." : "Generate"}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className={"p-5 h-full max-h-[calc(100vh-146px)] overflow-y-scroll"}>
+                            
 							<div>
-								<h4>Career Objective</h4>
-								<p>{objective ? objective : <span className={"text-xs"}>No data to display</span>}</p>
-							</div>
-							<div className={"mt-5"}>
-								<h4>Skills</h4>
-								<ListBox items={skills}/>
-							</div>
-							<div className={"mt-5"}>
-								<h4>Achievements</h4>
-								<ListBox items={Achievements}/>
-							</div>
-						</div>
-					</div>
-				</div>
+                                <h4>Title</h4>
+                                <p>{title ? title : <span className={"text-xs"}>No data to display</span>}</p>
+                            </div>
+                            <div className={"mt-5"}>
+                                <h4>Objectives</h4>
+                                <ListBox items={objectives} />
+                            </div>
+                            <div className={"mt-5"}>
+                                <h4>Actions</h4>
+                                <ListBox items={actions} />
+                            </div>
+                            <div className={"mt-5"}>
+                                <h4>Accounts Influenced</h4>
+                                <ListBox items={accountsInfluenced} />
+                            </div>
+                            <div className={"mt-5"}>
+                                <h4>Assets</h4>
+                                <ListBox items={assets} />
+                            </div>
+                            <div className={"mt-5"}>
+                                <h4>Impact</h4>
+                                <p>{impact ? impact : <span className={"text-xs"}>No data to display</span>}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 				<div className={"flex justify-between max-w-5xl mx-auto items-center py-4 border-t-2 px-5"}>
-					<div className={"text-sm uppercase text-purple-500"}>Rashvin all rights reserved 2023 &#169;</div>
-					<Link href={"https://github.com/pharavi"} target={"_blank"}>
-						<img src={"/github-mark.svg"} alt={"github-repo"} className={"w-7 h-7"}/>
-					</Link>
-				</div>
-			</main>
-			<Toaster/>
-		</>
-	)
+    			<div className={"text-sm uppercase text-purple-500 font-bold text-center flex-grow"}>
+        			powered by CME Marketing
+    			</div>
+    			<Link href={"https://github.com/pharavi"} target={"_blank"}>
+        		<img src={"/github-mark.svg"} alt={"github-repo"} className={"w-7 h-7"}/>
+    			</Link>
+					</div>
+            </main>
+            <Toaster />
+        </>
+    )
 }
